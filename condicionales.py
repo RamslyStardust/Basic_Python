ffa9b3020741d4cbd8377caf1c1dfb152eed3701
RamslyStardust <88069442+RamslyStardust@users.noreply.github.com>
2021-07-28 11:29:34 -0500

    Add files via upload

--- condicionales.py
+++ condicionales.py
@@ -1,6 +1,14 @@
 edad = int(input("Escribe tu edad: "))
 if edad > 17:
     print("Eres mayor de edad") 
+    # Siempre se identa con 4 espacios despues del if o funcion
 else:
     print("Eres menor de edad")
-
+numero =int(input("Escribe un numero: "))
+if numero > 5:
+    print("Es mayor a 5")
+elif numero == 5:
+    print("Es igual a  5")
+else:
+    print("Es menor a 5")
+    
