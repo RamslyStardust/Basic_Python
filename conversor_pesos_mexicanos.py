349b02fd71462e4ec222ead018af92ef63d86604
RamslyStardust <88069442+RamslyStardust@users.noreply.github.com>
2021-07-28 11:09:32 -0500

    Add files via upload

---
+++ conversor_pesos_mexicanos.py
@@ -0,0 +1,6 @@
+pesos = input("Ingrese su cantidad de dinero en pesos: ")
+pesos = float(pesos)
+dolares = pesos/20
+dolares = round(dolares, 2)
+dolares = str(dolares)
+print("La cantidad es: " + dolares + " dólares")
